277ed1e73886971e401334b100aff41fcab0adf8
unknown <cogitaciones1@gmail.com>
2024-02-15 01:47:37 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,19 @@
+import { Inter, Roboto_Mono ,Roboto,Lusitana} from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+ 
+
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
